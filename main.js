89ce44859390faf5f3e4d6fdd013f1d4e32bd798
sub <contact.sub22@gmail.com>
2022-03-20 20:47:26 +0700

    Fix show and hide status

--- main.js
+++ main.js
@@ -1,98 +1,113 @@
 
-document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+document.getElementById('task_form').addEventListener('submit', saveTask);
 
-function saveIssue(e) {
-  var issueDesc = document.getElementById('task_description_input').value;
-  var issueSeverity = document.getElementById('task_severity_input').value;
-  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-  var issueId = chance.guid();
-  var issueStatus = 'Open';
+function saveTask(e) {
+  var task_title = document.getElementById('task_title').value;
+  var task_servertity = document.getElementById('task_severity').value;
+  var task_description = document.getElementById('task_description').value;
+  var task_id = chance.guid();
+  var task_status = 'Open';
 
-  var issue = {
-    id: issueId,
-    description: issueDesc,
-    severity: issueSeverity,
-    assignedTo: issueAssignedTo,
-    status: issueStatus
+  var task = {
+    id: task_id,
+    task_title: task_title,
+    task_servertity: task_servertity,
+    task_description: task_description,
+    status: task_status
   }
 
-  var issues = [];
+  var tasks = [];
 
-  if (localStorage.getItem('issues') == null) {
-    issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
+  if (localStorage.getItem('tasks') == null) {
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(task));
   } else {
-    var issues = JSON.parse(localStorage.getItem('issues'));
-    issues.push(issue);
-    localStorage.setItem('issues', JSON.stringify(issues));
+    var tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 
-  document.getElementById('issueInputForm').reset();
+  document.getElementById('task_form').reset();
 
   fetchIssues();
 
   e.preventDefault();
 }
 
-function setStatusClosed(id) {
-  var issues = JSON.parse(localStorage.getItem('issues'));
-  for (var i = 0; i < issues.length; i++) {
-    if (issues[i].id == id) {
-      issues[i].status = 'Closed';
-    }
-  }
-
-  localStorage.setItem('issues', JSON.stringify(issues));
-
-  fetchIssues();
-}
-
 function deleteIssue(id) {
-  var issues = JSON.parse(localStorage.getItem('issues'));
-  for (var i = 0; i < issues.length; i++) {
-    if (issues[i].id == id) {
-      issues.splice(i, 1);
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id == id) {
+      tasks.splice(i, 1);
     }
   }
 
-  localStorage.setItem('issues', JSON.stringify(issues));
-
+  localStorage.setItem('tasks', JSON.stringify(tasks));
   fetchIssues();
 }
 
 function setStatusDone(id) {
-  var issues = JSON.parse(localStorage.getItem('issues'));
-  for (var i = 0; i < issues.length; i++) {
-    if (issues[i].id == id) {
-      issues[i].status = 'Done';
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id == id) {
+      tasks[i].status = 'Done';
     }
   }
 
-  localStorage.setItem('issues', JSON.stringify(issues));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  fetchIssues();
+}
 
+function setStatusClosed(id) {
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id == id) {
+      debugger
+      tasks[i].status = 'Closed';
+      $(".close-" + id).hide();
+      $(".open-" + id).show();
+    }
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  fetchIssues();
+}
+
+function setStatusOpen(id) {
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id == id) {
+      tasks[i].status = 'Open';
+      $(".close-" + id).show();
+      $(".open-" + id).hide();
+    }
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
   fetchIssues();
 }
 
 function fetchIssues() {
-  var issues = JSON.parse(localStorage.getItem('issues'));
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
 
-  var issuesList = document.getElementById('issuesList');
-  issuesList.innerHTML = '';
+  var tasksList = document.getElementById('task_list');
+  tasksList.innerHTML = '';
 
-  for (var i = 0; i < issues.length; i++) {
-    var id = issues[i].id;
-    var desc = issues[i].description;
-    var severity = issues[i].severity;
-    var assignedTo = issues[i].assignedTo;
-    var status = issues[i].status;
+  for (var i = 0; i < tasks.length; i++) {
+    var id = tasks[i].id;
+    var title = tasks[i].task_title;
+    var severity = tasks[i].task_servertity;
+    var description = tasks[i].task_description;
+    var status = tasks[i].status;
 
-    issuesList.innerHTML += '<div class="well">' +
+    tasksList.innerHTML += '<div class="well">' +
                         '<h6>Issue ID: ' + id + '</h6>' +
-                        '<p><span class="label label-info">' + status + '</span></p>' +
-                        '<h3>' + "Description: " + desc + '</h3>' +
+                        '<p><span class="label label-info status-'+id+'">' + status + '</span></p>' +
+                        '<h3>' + "Title: " + title + '</h3>' +
                         '<p><span class="glypicon glypicon-time"></span>' + 'Serverity: ' + severity + '</p>' +
-                        '<p><span class="glypicon glypicon-user"></span> ' + assignedTo + '</p>' +
-                        '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>' +
+                        '<p><span class="glypicon glypicon-user"></span>' + 'Description: ' + description + '</p>' +
+                        '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning close-'+id+'">Close</a>' +
+                        '<a href="#" onclick="setStatusOpen(\''+id+'\')" class="btn btn-warning open-'+id+'">Open</a>' +
                         '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger" style="margin: 20px">Delete</a>' +
                         '<a href="#" onclick="startTime(\''+id+'\')" class="btn btn-primary start-'+id+'">Start</a>' +
                         '<a href="#" onclick="stopTime(\''+id+'\')" class="btn btn-primary stop-'+id+'">Stop</a>' +
@@ -108,6 +123,15 @@ function fetchIssues() {
                           '</div>' +
                         '</div>';
     $(".stop-" + id).hide();
+    if(status == "Open") {
+      $(".open-" + id).hide();
+      $(".close-" + id).show();
+      $(".start-" + id).show();
+    } else {
+      $(".open-" + id).show();
+      $(".close-" + id).hide();
+      $(".start-" + id).hide();
+    }
   }
 }
 
@@ -127,6 +151,7 @@ var h = null;
 var m = null;
 var s = null;
 var timeout = null;
+
 function startTime(id) {
   if($("#s_val-" + id).val().length != 0) {
     $(".start-" + id).hide();
@@ -138,20 +163,15 @@ function startTime(id) {
     }
 
     if(s === -1){
-        m -= 1;
-        s = 59;
+      m -= 1;
+      s = 59;
     }
 
-    // Nếu số phút = -1 tức là đã chạy ngược hết số phút, lúc này:
-    //  - giảm số giờ xuống 1 đơn vị
-    //  - thiết lập số phút lại 59
     if(m === -1){
-        h -= 1;
-        m = 59;
+      h -= 1;
+      m = 59;
     }
 
-    // Nếu số giờ = -1 tức là đã hết giờ, lúc này:
-    //  - Dừng chương trình
     if(h.toString() == 'NaN') {
       h = 0;
       document.getElementById('h-' + id).innerText = 0;
@@ -173,7 +193,7 @@ function startTime(id) {
 
     if (h == 0 && m == 0 && s == 0){
       clearTimeout(timeout);
-      alert('TimeOut');
+      alert('The task be done');
       $(".start-" + id).show();
       $(".stop-" + id).hide();
       fetchIssues();
@@ -184,7 +204,7 @@ function startTime(id) {
       setStatusDone(id);
       return false;
     }
-    debugger
+
     timeout = setTimeout(function(){
       s--;
       startTime(id);
